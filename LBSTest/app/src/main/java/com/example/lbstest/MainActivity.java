9552484cfcf0987f11a7c88f90f5e7c02ef6b9f5
zhangyi109 <1439924953@qq.com>
2022-04-11 11:25:06 +0800

    初始版本的LBStest

--- LBSTest/app/src/main/java/com/example/lbstest/MainActivity.java
+++ LBSTest/app/src/main/java/com/example/lbstest/MainActivity.java
@@ -1,14 +1,127 @@
 package com.example.lbstest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public LocationClient mLocationClient;
+
+    private TextView positionText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+//        创建LocationClient实例，接收一个context参数
+        try {
+            mLocationClient = new LocationClient(getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        注册一个定位监听器，当获取位置时，就会回调这个定位监听器
+        mLocationClient.registerLocationListener(new MYLocationListener());
         setContentView(R.layout.activity_main);
+
+        positionText = (TextView) findViewById(R.id.position_tv);
+//        获取相关权限
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        //判断permissionList是否为空，否则申请的权限不满足，将不能继续执行程序
+        if (!permissionList.isEmpty()){
+//            一次性申请权限
+            String [] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this,permissions,1);
+        }
+        else{
+            requestLocation();
+        }
     }
+    public void requestLocation(){
+        mLocationClient.start();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0){
+                    for (int result :grantResults){
+                        if (result != PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(this, "必须同意所有权限才能使用此程序", Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    requestLocation();
+                }else{
+                    Toast.makeText(this, "未知错误", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    public class  MYLocationListener extends BDAbstractLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    StringBuilder currentPosition = new StringBuilder();
+
+//                    添加数据
+                    currentPosition.append("latitude: ").append(bdLocation.getLatitude());
+                    currentPosition.append("longtitude : ").append(bdLocation.getLongitude());
+                    currentPosition.append("定位方式： ");
+                    if (bdLocation.getLocType() == BDLocation.TypeGpsLocation){
+                        currentPosition.append("GPS");
+                    }
+                    else if (bdLocation.getLocType() == BDLocation.TypeOffLineLocation){
+                        currentPosition.append("Network");
+                    }
+                    positionText.setText(currentPosition);
+
+
+                }
+            });
+        }
+
+        @Override
+        public void onConnectHotSpotMessage(String s, int i) {
+            super.onConnectHotSpotMessage(s, i);
+        }
+    }
+
 }
